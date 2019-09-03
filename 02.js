d63c33b32e606c30369f42f5a813886cb84c925e
Ocean <hallieliu123@163.com>
2019-09-04 03:53:01 +0800

    add from new1

--- 02.js
+++ 02.js
@@ -1,2 +1,11 @@
 // 456
 // 789
+function test(){
+    // test merge
+    var arr = [];
+    for( var i = 0; i< 6; i++ ){
+           arr[i] = (function(i){
+               console.log(i)
+           })(i)
+    }
+}
