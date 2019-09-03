87a93959b960eff857a0c2be733503e82f181b01
Ocean <hallieliu123@163.com>
2019-09-04 03:51:06 +0800

    add from master

--- 02.js
+++ 02.js
@@ -1,2 +1,12 @@
 // 456
 // 789
+function test(){
+    // test merge
+    let arr = [];
+    for( let i = 0; i< 6; i++ ){
+           arr[i] = function(){
+               console.log(i)
+           }
+    }
+}
+
