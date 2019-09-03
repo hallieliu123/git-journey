4e43fd3d79fa015fdc6fa1f5df4886cb0c1f0633
Ocean <hallieliu123@163.com>
2019-09-03 09:58:03 +0800

    test1

---
+++ 01.js
@@ -0,0 +1,35 @@
+// 查看用户信息
+
+// git config user.name
+// git config use.email
+
+// 修改用户
+
+// git config --global user.name 'Ocean'
+// git config --global user.email 'hallieliu123@163.com'
+
+
+// 工作区   暂存区   版本库 
+
+
+// 操作命令
+
+// 1. git clone < URL >  
+
+// 2. git status 对比工作区和暂存区代码
+
+// 3. git add < filename > 添加此更改文件代码  || git add. 添加所有更改文件代码
+
+// 4. git commit -m 'memo' 暂存区提交至版本库
+
+// 5. git commit -a -m 'memo' 工作区同时更新到暂存区和版本库
+
+// 6. git log 查看提交记录
+
+
+
+
+
+
+
+
